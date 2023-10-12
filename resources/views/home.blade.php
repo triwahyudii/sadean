3e24aec33b04edae8220b2ba9ba8f38b03e4d1a8
tri wahyudi <mesirah6@gmail.com>
2023-10-12 15:15:54 +0700

    add: floating button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -185,10 +185,165 @@
             </div>
         </section>
 
-      
+        <!-- Footer -->
+        <footer class="text-center text-lg-start bg-white text-muted">
+            <!-- Section: Social media -->
+            <!-- <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </section> -->
+            <!-- Section: Social media -->
+
+            <!-- Section: Links  -->
+            <section class="pt-1">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <i class="fas fa-gem me-3 text-secondary"></i>Sadean
+                            </h6>
+                            <p>
+                                Kami berpengalaman lebih dari 8 tahun. <br /> Sadean adalah para ahli dalam bidangnya, pembanguna rumah, penyedia alat pesta dan alat pernikahan, pemasangan gorden dan dengan transparansi dalam anggaran akan memudahkan proses pengerjaan.
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Jasa Kami
+                            </h6>
+                            <p>
+                                <a href="#!" class="text-reset">Jasa Bangun Rumah</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Sewa Alat Pesta</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-reset">Gorden</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">Kontak Kami</h6>
+                            <p><i class="fas fa-home me-3 text-secondary"></i> Ponggok, Blitar, Jawa Timur</p>
+                            <p>
+                                <i class="fas fa-envelope me-3 text-secondary"></i>
+                                trideveloper99@gmail.com
+                            </p>
+                            <p><i class="fas fa-phone me-3 text-secondary"></i> +6285732003433</p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+
+            <!-- Copyright -->
+            <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+                © 2023 Copyright
+                <a class="text-reset fw-bold" href="#">sadean.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+    </div>
+
+    <!-- Floating Buttons Whatsapp -->
+    <div id="whatsapp">
+        <a href="#" target="_blank" id="toggle1" class="wtsapp">
+            <i class="fa fa-whatsapp"></i>
+        </a>
     </div>
 
     <script src="https://kit.fontawesome.com/39d2ff4747.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
+
+<style type="text/css">
+    #whatsapp .wtsapp:focus {
+        border: none;
+        outline: none;
+    }
+
+    #whatsapp .wtsapp {
+        position: fixed;
+        transition: all .5s ease;
+        background: #25d366;
+        display: block;
+        text-align: center;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+        border-radius: 50px;
+        border-right: none;
+        color: #fff;
+        font-weight: 700;
+        font-size: 18px;
+        bottom: 130px;
+        right: 20px;
+        border: 0;
+        z-index: 99;
+        width: 50px;
+        height: 50px;
+        line-height: 48px;
+    }
+
+    #whatsapp .wtsapp:before {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        left: 50%;
+        top: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        display: block;
+        width: 60px;
+        height: 60px;
+        background: #25d366;
+        border-radius: 50%;
+        -webkit-animation: pulse-border 1500ms ease-out infinite;
+        animation: pulse-border 1500ms ease-out infinite;
+    }
+
+    @keyframes pulse-border {
+
+        0% {
+            transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1);
+            opacity: 1;
+        }
+
+        100% {
+            transform: translateX(-50%) translateY(-50%) translateZ(0) scale(1.5);
+            opacity: 0;
+        }
+
+    }
+</style>
